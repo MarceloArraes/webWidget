cbd9c0c22f7b94933bbbdf7e22bd997c56df95cd
marcelo <marcelo.arraes@gmail.com>
2022-05-04 11:55:40 +0000

    feat: add three buttons and made a behavior that when clicked it shows hello world. need to fix the close button position.

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,10 +1,13 @@
 import { ChatTeardropDots } from "phosphor-react";
 import { Popover } from "@headlessui/react";
+import { WidgetForm } from "./WidgetForm";
 
 export function Widget() {
   return (
-    <Popover className="absolute bottom-4 right-4">
-      <Popover.Panel className="text-4xl">Hello WORLD</Popover.Panel>
+    <Popover className="absolute bottom-4 right-4 sm:bottom-8 sm:right-8 flex flex-col items-end">
+      <Popover.Panel className="text-4xl">
+        <WidgetForm />
+      </Popover.Panel>
       <Popover.Button className="flex items-center bg-violet-500 rounded-full h-12  px-3 text-white group">
         <ChatTeardropDots className="items-center h-6 w-6" />
         <span className="transition-all ease-in-out duration-700 overflow-hidden max-w-0 group-hover:max-w-xs group-focus:max-w-xs ">
